68aebc4f2a604ac366ee0093ffd8dd77efce5262
nehldotjs <nelsonosuya11@gmail.com>
2023-12-03 00:41:39 +0100

    NEW COMMIT

--- Components/StackScreens.js
+++ Components/StackScreens.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 // FIREBASE IMPORTS
@@ -12,25 +12,56 @@ import { useFirebaseContext } from "../ContextProviders/FirebaseAuth";
 // CONTEXT IMPORTS
 
 // SCREENS IMPORT
-
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import RegistrationScreen from "./screens/RegistrationScreen";
-
 // SCREENS IMPORT
 
 const Stack = createStackNavigator();
 
 const StackScreens = () => {
-  // const { text, onAuthState } = useFirebaseContext();
+  const { onAuthState, setAuthState } = useFirebaseContext();
+  const [loading, setLoading] = useState(true);
+
+  const auth = FIREBASE_AUTH;
+
+  useEffect(() => {
+    console.log("useEffect - Setting up auth state change listener");
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setLoading(false); // Set loading to false once the authentication state is determined
+      if (user) {
+        setAuthState(true);
+      } else {
+        setAuthState(false);
+      }
+    });
+
+    return () => {
+      console.log("Cleaning up auth state change listener");
+      unsubscribe();
+    };
+  }, []);
+
+  if (loading) {
+    // You might want to show a loading indicator or splash screen here
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <>
-      <Stack.Navigator>
+    <Stack.Navigator>
+      {onAuthState ? (
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Create-account" component={RegistrationScreen} />
-      </Stack.Navigator>
-    </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Create-account" component={RegistrationScreen} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 };
 
--- ContextProviders/FirebaseAuth.js
+++ ContextProviders/FirebaseAuth.js
@@ -1,31 +1,14 @@
-import React, { useContext, useState, useEffect, createContext } from "react";
+import React, { useContext, createContext,useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { FIREBASE_AUTH } from "../firebase";
 
 const firebaseContextProvider = createContext();
 const fireAuth = () => {
-  const [onAuthState, setAuthState] = useState(true);
-  const auth = FIREBASE_AUTH;
-  console.log(auth);
-  useEffect(() => {
-    console.log("useEffect - Setting up auth state change listener");
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      console.log("onAuthStateChanged - User changed:", user);
-      if (user) {
-        setAuthState(true);
-        console.log("USER LOGGED IN :", onAuthState);
-      }
-    });
-
-    return () => {
-      console.log("Cleaning up auth state change listener");
-      unsubscribe();
-    };
-  }, []);
+  const [onAuthState, setAuthState] = useState(null); // Change initial state to null
 
   const result = {
     text: "firebase auth provider",
-    onAuthState
+    onAuthState,
+    setAuthState
   };
   return result;
 };
@@ -47,5 +30,4 @@ function useFirebaseContext() {
   }
   return baseContext;
 }
-
 export { FirebaseAuth, useFirebaseContext };
